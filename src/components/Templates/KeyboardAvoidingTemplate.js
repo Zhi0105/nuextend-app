da8a6bcf8a8959f6538878cd0d232a42c6f36cde
INQ-argie <argie.barcena@inquirer.net>
2025-06-10 11:20:42 +0800

    fix keyboard avoiding

--- src/components/Templates/KeyboardAvoidingTemplate.js
+++ src/components/Templates/KeyboardAvoidingTemplate.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import { KeyboardAvoidingView, Platform } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const KeyboardAvoidingTemplate = ({ children }) => {
     return (
-        <SafeAreaView
-            style={{ flex: 1, backgroundColor: '#fff' }}
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
         >
-            <KeyboardAvoidingView
-                style={{ flex: 1, backgroundColor: '#EEEEEE' }}
-                behavior={Platform.OS === 'ios' ? 'padding' : 'position'}
-                keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : -120} // adjust the value according to your needs
-            >
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1, backgroundColor: '#EEEEEE', padding: 16 }}
+                keyboardShouldPersistTaps="handled"
+                >
                 {children}
-            </KeyboardAvoidingView>
+            </ScrollView>
+        </KeyboardAvoidingView>
         </SafeAreaView>
-    )
-}
+    );
+};
